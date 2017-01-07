06066a028be7f1ce5e7811680f209b16ebb57dba
betasynth <betasynth@gmail.com>
2017-01-07 22:35:11 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function () {
                     showVideo();
                 };
                              
-                
+                draw();
                 
             },
             // Error Callback

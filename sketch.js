da9b17cc05d7c5dfd44df6848b2e04a79c237c8e
betasynth <betasynth@gmail.com>
2017-01-07 23:28:32 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -103,6 +103,25 @@ for (var k=0; k<10; k++) {
   x0=mouseX;
   y0=mouseY;
   
+  
+  
+  
+  
+  
+  
+  
+   capture.loadPixels();
+  var stepSize = round(constrain(mouseX / 8, 6, 32));
+  for (var y=0; y<height; y+=stepSize) {
+    for (var x=0; x<width; x+=stepSize) {
+      var i = y * width + x;
+      var darkness = (255 - capture.pixels[i*4]) / 255;
+      var radius = stepSize * darkness;
+      ellipse(x, y, radius, radius);
+    }
+  }
+  
+  
   return false;
 }
 
